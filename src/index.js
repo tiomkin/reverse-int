4f436818b33de8bf2af9c4dc9df44d3e94296c27
Artsiom Burdukou <artsiomburdukou@gmail.com>
2021-08-23 16:39:22 +0300

    feat: implement reverse method

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 module.exports = function reverse (n) {
-  
+  let output = '';
+  n = Math.abs(n);
+  while (n > 0) {
+      let digit = n % 10;
+      output += digit;
+      n = Math.floor(n / 10);
+  }
+
+  return output;
 }
